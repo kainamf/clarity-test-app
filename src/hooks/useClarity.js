da83bb716b566ae7cf0d368b7737ac06d07db1f2
kainamf <kaina.marques@calindra.com.br>
2025-03-26 13:45:44 -0300

    Adicionando Microsoft Clarity

---
+++ src/hooks/useClarity.js
@@ -0,0 +1,22 @@
+import { useEffect } from "react";
+
+const useClarity = (clarityId) => {
+  useEffect(() => {
+    if (!clarityId) return;
+
+    (function (c, l, a, r, i, t, y) {
+      c[a] =
+        c[a] ||
+        function () {
+          (c[a].q = c[a].q || []).push(arguments);
+        };
+      t = l.createElement(r);
+      t.async = 1;
+      t.src = "https://www.clarity.ms/tag/" + i;
+      y = l.getElementsByTagName(r)[0];
+      y.parentNode.insertBefore(t, y);
+    })(window, document, "clarity", "script", clarityId);
+  }, [clarityId]);
+};
+
+export default useClarity;
